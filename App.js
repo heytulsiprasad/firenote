7b6ebdd8b05111f101ba46e698615d9c8ba08ba4
Tulsi Prasad <tulsi.prasad50@gmail.com>
2021-07-26 13:43:47 +0530

    Fetch one-time data from firestore

--- App.js
+++ App.js
@@ -1,11 +1,26 @@
 import React from 'react';
 import {SafeAreaView, StyleSheet, Text, View} from 'react-native';
+import firestore from '@react-native-firebase/firestore';
 
 const App = () => {
+  const [users, setUsers] = React.useState([]);
+
+  React.useEffect(() => {
+    firestore()
+      .collection('users')
+      .get()
+      .then(data => {
+        data.docs.forEach(doc => {
+          setUsers(c => [...c, doc.data()]);
+        });
+      });
+  }, []);
+
   return (
     <SafeAreaView style={styles.safeArea}>
       <View style={styles.view}>
         <Text>Homescreen</Text>
+        <Text>{JSON.stringify({users}, null, 4)}</Text>
       </View>
     </SafeAreaView>
   );
